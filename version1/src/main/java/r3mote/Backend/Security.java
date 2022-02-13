760fcb1a4eadd171e9ad61d4a2ee8e8eb2a25850
r3mot github <88360543+r3mot@users.noreply.github.com>
2022-02-12 18:37:18 -0800

    Updated databse blass name. Developed password encryption

---
+++ version1/src/main/java/r3mote/Backend/Security.java
@@ -0,0 +1,54 @@
+package r3mote.SQL;
+
+
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+
+
+public class Security {
+    
+    private SecretKey key;
+    private int KEY_SIZE = 128;
+    private Cipher encryptionCipher;
+    private int T_LEN = 128;
+
+
+    public void init() throws Exception {
+        KeyGenerator generator = KeyGenerator.getInstance("AES");
+        generator.init(KEY_SIZE);
+        key = generator.generateKey();
+
+    }
+    
+    public String encryptPassword(String rawPassword) throws Exception{
+
+        byte[] passwordInBytes = rawPassword.getBytes();
+        encryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        encryptionCipher.init(Cipher.ENCRYPT_MODE, key);
+        byte[] encryptedBytes = encryptionCipher.doFinal(passwordInBytes);
+        return encode(encryptedBytes);
+
+    }
+
+    public String decryptPassword(String encryptedPassword) throws Exception {
+
+        byte[] passwordInBytes = decode(encryptedPassword);
+        Cipher decryptionCipher = Cipher.getInstance("AES/GCM/NoPadding");
+        GCMParameterSpec spec = new GCMParameterSpec(T_LEN, encryptionCipher.getIV());
+        decryptionCipher.init(Cipher.DECRYPT_MODE, key, spec);
+        byte[] decryptedBytes = decryptionCipher.doFinal(passwordInBytes);
+        return new String(decryptedBytes);
+
+    }
+    private byte[] decode(String data){
+        return Base64.getDecoder().decode(data);
+    }
+
+    private String encode(byte[] data){
+        return Base64.getEncoder().encodeToString(data);
+    }
+}
