812602a775e68120bfdd6d07085c81589d81b2bf
r3mot github <88360543+r3mot@users.noreply.github.com>
2022-02-12 17:48:17 -0800

    Added dependency for SQLite. Added javafx graphics to module

--- version1/src/main/java/module-info.java
+++ version1/src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module r3mote {
+    requires transitive javafx.graphics;
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
+    
     opens r3mote to javafx.fxml;
     exports r3mote;
 }
